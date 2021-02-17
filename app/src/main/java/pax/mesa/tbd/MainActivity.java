50cb9bf73af759c7b55349983734c33547020c94
Evan <evangearhart@icloud.com>
2021-02-17 15:26:25 -0500

    Login, register, and logout capabilities complete. Need to add classlist for registering new users. Database implementation also now working. Added a user class to store info for database.

--- app/src/main/java/pax/mesa/tbd/MainActivity.java
+++ app/src/main/java/pax/mesa/tbd/MainActivity.java
@@ -78,12 +78,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()) {
-            case R.id.action_logout:
-                logoutUser();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.action_logout) {
+            logoutUser();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
         }
     }
 
